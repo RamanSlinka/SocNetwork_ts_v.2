53d61c1a915bbbd245e320f3e2a5a751f75c69df
RamanSlinka <romuch1986@gmail.com>
2022-10-30 20:20:17 +0100

    restarted the project...

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,12 @@
-
-import React from "react";
+import React from 'react';
 import './App.css';
-import Navbar from "./Component/Navbar/Navbar";
-import Header from './Component/Header/Header';
-import Dialogs from "./Component/Dialogs/Dialogs";
-import Profile from "./Component/Profile/Profile";
-import {BrowserRouter, Route} from 'react-router-dom'
 
 function App() {
-    return (
-        <BrowserRouter>
-        <div className="AppBG">
-            <div className="app-wrapper">
-                <Header/>
-                <Navbar/>
-                <div className='app-wrapper-content'>
-                    <Route path='/profile' component={ Profile}/>
-                    <Route path='/dialogs' component={ Dialogs}/>
-
-                </div>
-            </div>
-        </div>
-        </BrowserRouter>
-    );
+  return (
+    <div className="App">
+     Hello
+    </div>
+  );
 }
 
 export default App;
