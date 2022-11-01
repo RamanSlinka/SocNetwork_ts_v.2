de794f71bace2471ac5ec750224c76adcdcb9f8c
RamanSlinka <romuch1986@gmail.com>
2022-11-01 18:06:31 +0100

    Added Dialogs components/ +  style modules for components. Added React-Router-Dom to project

--- src/components/dialogs/Dialogs.tsx
+++ src/components/dialogs/Dialogs.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import style from './dialogs.module.scss'
+import {NavLink} from "react-router-dom";
 
 const Dialogs = () => {
     return (
-        <div>
-            dialogs
+        <div className={style.dialogs}>
+            <div className={style.users}>
+                <NavLink to='/dialogs/1' activeClassName={style.active} className={style.user}>User1</NavLink>
+                <NavLink to='/dialogs/2' activeClassName={style.active} className={style.user}>User2</NavLink>
+                <NavLink to='/dialogs/3' activeClassName={style.active} className={style.user}>User3</NavLink>
+            </div>
+            <div className={style.messages}>
+                <div className={style.message}>message1</div>
+                <div className={style.message}>message2</div>
+            </div>
         </div>
     );
 };
