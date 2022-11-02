6e135e5fbb448058593e80369e18ff77697ac4fc
RamanSlinka <romuch1986@gmail.com>
2022-11-02 22:00:10 +0100

    separated on BLL and UI; used Array.map

--- src/components/dialogs/Dialogs.tsx
+++ src/components/dialogs/Dialogs.tsx
@@ -1,20 +1,36 @@
 import React from 'react';
-import style from './dialogs.module.scss'
-import {NavLink} from "react-router-dom";
+import style from './dialogs.module.scss';
 import DialogItem from "./dialogItem/DialogItem";
 import Message from "./message/Message";
 
 const Dialogs = () => {
+
+    const dialogsData = [
+        {user:'User1', id:'1'},
+        {user:'User2', id:'2'},
+        {user:'User3', id:'3'},
+    ]
+    const messagesData = [
+        {message:'message1', id:'1'},
+        {message:'message2', id:'2'},
+        {message:'message3', id:'3'},
+
+    ]
+
     return (
         <div className={style.dialogs}>
             <div>
-                <DialogItem user='User1' id='1'/>
-                <DialogItem user='User2' id='2'/>
-                <DialogItem user='User3' id='3'/>
+                {dialogsData.map((dialog, index) => (
+                    <DialogItem user={dialog.user} id={dialog.id} key={index}/>
+                ))}
+
             </div>
         <div>
-            <Message message='message1'/>
-            <Message message='message2'/>
+            {messagesData.map((message, index) => (
+                <Message message={message.message} key={index}/>
+            ))}
+
+
         </div>
         </div>
     );
--- src/components/profile/myPosts/MyPosts.tsx
+++ src/components/profile/myPosts/MyPosts.tsx
@@ -2,6 +2,11 @@ import React from 'react';
 import Post from "./post/Post";
 
 const MyPosts = () => {
+
+    const postsData = [
+        {message:'message 1', likesCount:'1', id: 1},
+        {message:'message 2', likesCount:'3', id: 2},
+    ]
     return (
         <div>
             <div>My posts</div>
@@ -9,8 +14,11 @@ const MyPosts = () => {
                 <textarea></textarea>
                 <button> New post</button>
                </div>
-            <Post message='message 1' likesCount='1' />
-            <Post message='message 2' likesCount='2' />
+            {postsData.map((post) => (
+                <Post message={post.message} likesCount={post.likesCount} key={post.id} />
+
+            ))}
+
         </div>
     );
 };
