32f22e64d8618a93a6f438981fed0ff86ef41141
RamanSlinka <romuch1986@gmail.com>
2022-11-23 21:19:17 +0100

    Updated profile reducer (added user profile response)

--- src/ redux/profileReducer.ts
+++ src/ redux/profileReducer.ts
@@ -9,9 +9,33 @@ export type PostType = {
     id: number
 }
 
+export type UserProfileType = {
+    aboutMe: string | null
+    contacts: {
+        facebook: string | null
+        website: string | null
+        vk: string | null
+        twitter: string | null
+        instagram: string | null
+        youtube: string | null
+        github: string | null
+        mainLink: string | null
+    },
+    lookingForAJob: boolean
+    lookingForAJobDescription: string | null
+    fullName: string | null
+    userId: number
+    photos: {
+        small: string | null
+        large: string | null
+    }
+}
+
+
 type InitialStateType = {
     posts: PostType[]
     newPostText: string | undefined
+    userProfile: UserProfileType | null
 }
 
 const posts = [
@@ -21,7 +45,8 @@ const posts = [
 
 const initialState: InitialStateType = {
     posts: posts,
-    newPostText: ''
+    newPostText: '',
+    userProfile: null
 }
 
 const profileReducer = (state = initialState, action: ProfileActionType): InitialStateType => {
@@ -29,18 +54,23 @@ const profileReducer = (state = initialState, action: ProfileActionType): Initia
     switch (action.type) {
         case 'ADD-POST': {
             const newPost = state.newPostText
-                        return {
-                            ...state,
-                            newPostText: '',
-                            posts: [
-                                {message: newPost, likesCount: '0', id: 5},
-                                ...state.posts]
-                        };
+            return {
+                ...state,
+                newPostText: '',
+                posts: [
+                    {message: newPost, likesCount: '0', id: 5},
+                    ...state.posts]
+            };
         }
 
         case 'UPDATE-NEW-POST-TEXT': {
             return {...state, newPostText: action.newText};
         }
+        case "SET-USER-PROFILE":
+            return {
+                ...state,
+                userProfile: action.profile
+            }
 
         default:
             return state
@@ -51,9 +81,12 @@ export default profileReducer;
 
 export type AddPostACType = ReturnType<typeof addPost>
 export type UpdateNewPostTextACType = ReturnType<typeof updateNewPostText>
+export type SetUserProfileACType = ReturnType<typeof setUserProfileAC>
 
-export type ProfileActionType = AddPostACType | UpdateNewPostTextACType
+export type ProfileActionType = AddPostACType
+    | UpdateNewPostTextACType
+    | SetUserProfileACType
 
 export const addPost = () => ({type: 'ADD-POST'} as const);
-export const updateNewPostText = (text: string | undefined) =>
-    ({type: 'UPDATE-NEW-POST-TEXT', newText: text} as const);
+export const updateNewPostText = (text: string | undefined) => ({type: 'UPDATE-NEW-POST-TEXT', newText: text} as const);
+export const setUserProfileAC = (profile: UserProfileType) => ({type: 'SET-USER-PROFILE', profile} as const)
