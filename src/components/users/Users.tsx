698690c95b98a7d22ae26f7e9be6d8956be9e101
RamanSlinka <romuch1986@gmail.com>
2022-11-27 15:59:06 +0100

    updated Users component : added follow/unfollow associated with the request

--- src/components/users/Users.tsx
+++ src/components/users/Users.tsx
@@ -13,6 +13,7 @@ import styles from './users.module.scss'
 import axios from "axios";
 import {Pagination, PaginationProps} from 'antd';
 import Preloader from "../common/Preloader";
+import {NavLink} from "react-router-dom";
 
 const noImage = 'https://st2.depositphotos.com/1009634/7235/v/450/depositphotos_72350117-stock-illustration-no-user-profile-picture-hand.jpg'
 
@@ -24,12 +25,13 @@ const Users = () => {
     const currentPage = useSelector<AppStateType, number>(state => state.usersPage.currentPage)
     const pageSize = useSelector<AppStateType, number>(state => state.usersPage.pageSize)
     const isFetching = useSelector<AppStateType, boolean>(state => state.usersPage.isFetching)
-    console.log(users)
+    // console.log(users)
     const dispatch = useDispatch()
 
     useEffect(() => {
         dispatch(toggleIsFetchingAC(true))
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${currentPage}&count=${pageSize}`)
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${currentPage}&count=${pageSize}`,
+            {withCredentials: true})
             .then((res) => {
                 const usersData = res.data.items;
                 dispatch(setUsersAC(usersData))
@@ -43,7 +45,7 @@ const Users = () => {
 
 
     const changeActivePage = (selectedPage: number) => {
-      dispatch(setCurrentPageAC(selectedPage))
+        dispatch(setCurrentPageAC(selectedPage))
     }
 
     // const pagesCount = Math.ceil(totalUsersCount / pageSize)
@@ -51,7 +53,6 @@ const Users = () => {
     // for(let i = 1; i < pagesCount; i++) {
     //     pages.push(i)
     // }
-
 
 
     const itemRender: PaginationProps['itemRender'] = (_, type, originalElement) => {
@@ -65,12 +66,11 @@ const Users = () => {
     };
 
 
-
     return (
         <>
-            { isFetching && <Preloader/>}
+            {isFetching && <Preloader/>}
             <Pagination
-                 onChange={changeActivePage}
+                onChange={changeActivePage}
                 // onShowSizeChange={changeActivePage}  //Called when pageSize is changed	function(current, size)
                 current={currentPage}
                 total={totalUsersCount}
@@ -81,8 +81,10 @@ const Users = () => {
             {users && users.map((u: User) => (
                 <div key={u.id}>
                     <div style={{display: 'flex', flexDirection: 'row'}}>
-                        <img src={u.photos.small ? u.photos.small : noImage} alt="user photo"
-                             style={{width: '80px', margin: '10px'}}/>
+                        <NavLink to={'/profile'}>
+                            <img src={u.photos.small ? u.photos.small : noImage} alt="user photo"
+                                 style={{width: '80px', margin: '10px'}}/>
+                        </NavLink>
                         <div>
                             <p>User : </p>
                             <p>{u.name}</p>
@@ -91,8 +93,35 @@ const Users = () => {
                     </div>
                     <div>
                         {u.followed
-                            ? <button onClick={() => dispatch(setUnfollowedAC(u.id))}>Unfollow</button>
-                            : <button onClick={() => dispatch(setFollowedAC(u.id))}>Follow</button>
+                            ? <button onClick={() => {
+                                axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/ ${u.id}`,
+                                    {withCredentials: true,
+                                        headers:  {'API-KEY' : 'e98254e3-f006-4d39-b52d-ad99d83a542e'
+                                    }},
+                                   )
+                                    .then((res) => {
+                                        if (res.data.resultCode === 0) {
+                                            dispatch(setUnfollowedAC(u.id))
+                                        }
+                                    })
+
+                            }
+
+
+                            }>Unfollow</button>
+                            : <button onClick={() => {
+                                axios.post(`https://social-network.samuraijs.com/api/1.0/follow/ ${u.id}`,{},
+                                    {withCredentials: true,
+                                        headers: {
+                                            'API-KEY': 'e98254e3-f006-4d39-b52d-ad99d83a542e'
+                                        }})
+                                    .then((res) => {
+                                        if (res.data.resultCode === 0) {
+                                            dispatch(setFollowedAC(u.id))
+                                        }
+                                    })
+                            }
+                            }>Follow</button>
                         }
 
                     </div>
